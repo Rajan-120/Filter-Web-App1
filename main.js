208422759496691db79969d999fc352c073c9305
Rajan-120 <78260275+Rajan-120@users.noreply.github.com>
2022-11-23 19:20:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload()
+{}
+
+function setup()
+{
+    canvas = createCanvas(350, 300);
+    canvas.position(505, 325);
+}
+
+function draw()
+{}
+
+function take_snapshot()
+{
+    save("Filtered Image.png");
+}
